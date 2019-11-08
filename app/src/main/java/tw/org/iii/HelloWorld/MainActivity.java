47ff3ccf01f7453f1609bba64fc96745480d10a8
DCH <dching0121@gmail.com>
2019-11-08 13:46:31 +0800

    clickListener & Log

--- app/src/main/java/tw/org/iii/HelloWorld/MainActivity.java
+++ app/src/main/java/tw/org/iii/HelloWorld/MainActivity.java
@@ -3,12 +3,26 @@ package tw.org.iii.HelloWorld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
+    private Button buttonClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //呼叫res/layout/activity_main.xml
+
+        textView = findViewById(R.id.lottrey);
+        buttonClick = findViewById(R.id.click);
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("DCH", "OK");
+            }
+        });
     }
 }
